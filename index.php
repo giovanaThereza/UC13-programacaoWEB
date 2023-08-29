dcd501822def95b630632cda0cfac4e7d6d7a2dc
Giovana Thereza <134647990+giovanaThereza@users.noreply.github.com>
2023-08-29 16:55:14 -0300

    coisas novas

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Bootstrap</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    </head>
+        <body>
+
+<div class="container">
+        <div class="text-bg-primary pb-2 mt-3 rounded-3 text-center">
+                <h1>Aula Bootstrap</h1> 
+        </div>
+
+        <div class="row">
+            <div class="text-bg-dark rounded col-12 col-md-4 text-center">
+                    <h2>Coluna 1</h2>
+            </div>
+
+            <div class="text-bg-info rounded col-6 col-md-4 text-center">
+                    <h2>Coluna 2</h2>
+            </div>
+
+            <div class="text-bg-success rounded col-6 col-md-4 text-center">
+                    <h2>Coluna 3</h2>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="text-bg-secondary rounded text-center">
+                <h2>Coluna 4</h2>
+            </div>
+
+            <div class="text-bg-warning rounded col-12 col-md-6 text-center ">
+                <h2>Coluna 5</h2>
+            </div>
+
+            <div class="text-bg-dark rounded col-6 text-center d-none d-md-block">
+                <h2>Coluna 6 </h2>
+            </div>
+
+            <div class="text-bg-danger rounded text-center  ">
+                <h2>Coluna 7</h2>
+            </div>
+        </div>
+        
+       
+</div>        
+
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"> </script>
+        </body>
+</html>
